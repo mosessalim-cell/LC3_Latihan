07d0fada88af7bf30d1bbe0b6760919e7449f13e
mosessalim-cell <moses.salim@gmail.com>
2026-02-01 16:24:39 +0700

    latihan api

--- main.py
+++ main.py
@@ -16,22 +16,5 @@ def get_all_data():
         "total_data": len(data),
         "data": data
     }
-
-
-# DELETE: hapus data by id
-@app.delete("/data/{item_id}")
-def delete_data(item_id: int):
-    global data
-
-    for i, item in enumerate(data):
-        if item["id"] == item_id:
-            deleted = data.pop(i)
-            return {
-                "message": "Data berhasil dihapus",
-                "deleted_data": deleted
-            }
-
-    raise HTTPException(status_code=404, detail="Data tidak ditemukan")
-
 # Ngerun API nya, run di terminal
 # uvicorn main:app --reload
